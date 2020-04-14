11b1920db97904d407ed5ac0e82d62b2e6d90ed0
bakate <bakateba@gmail.com>
2020-04-14 13:06:13 +0200

    config file updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby`,
         short_name: `starter`,
         start_url: `/`,
         // background_color: `#663399`,
