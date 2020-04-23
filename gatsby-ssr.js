7cc502ed5debd6695cd72ae771e109f594307d16
bakate <bakateba@gmail.com>
2020-04-23 10:21:22 +0200

    updated the gatsby-ssr file

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,16 @@
-import React from "react"
-import { CartStateProvider } from "./src/components/context/LocalState"
+import '@aws-amplify/ui/dist/style.css';
+import Amplify from 'aws-amplify';
+import React from "react";
+import awsConfig from './src/aws-exports';
+import { CartStateProvider } from "./src/components/context/LocalState";
+Amplify.configure(awsConfig)
+
 export const wrapRootElement = ({ element }) => {
-  return (<CartStateProvider>
-  {element}
-    </CartStateProvider>)
+
+  return (
+    <CartStateProvider>
+      {element}
+    </CartStateProvider>
+
+  )
 }
